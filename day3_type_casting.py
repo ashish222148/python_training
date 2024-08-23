2795affd8407c7c34bcc40f55ebf812b6123af6e
ashishailiya222148 <ashishailiya222148@gmail.com>
2024-08-23 06:35:48 +0530

    type casting with example

---
+++ day3_type_casting.py
@@ -0,0 +1,17 @@
+#type casting
+# x=input("enter the number: ")
+# print(x,type(x))
+
+# y=int(input("enter the first number:"))
+# print(y,type(y))
+
+#int,float,bool,str,complex
+print(int(12.5))
+print(int(True))
+print(float(10))
+print(int('45')) #only [0-9] can be coverted in to int
+
+print(bool(0)) #only in case of o, it will return as false , else it will always return True
+print(bool('')) #only empty string will return false , else it will always return True
+print(str(False))
+print(str(40.5))
