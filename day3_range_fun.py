4b6257ba77e2cb9d03c9b79ffb498b72e277bbe2
ashishailiya222148 <ashishailiya222148@gmail.com>
2024-08-23 09:15:39 +0530

    range function

---
+++ day3_range_fun.py
@@ -0,0 +1,10 @@
+#range(start,end,steps)
+#start default 0, end always end -1(last step), steps by default +1
+#range(5)->0,1,2,3,4
+#range(2,5)->2,3,4
+#range(2,20,2)->2,4,6,8,10,12,14,16,18
+#range(5,1,-1)->5,4,3,2
+print(list(range(5)))
+print(list(range(2,5)))
+print(list(range(2,20,5)))
+print(list(range(10,1,-1)))
